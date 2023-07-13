7374b68a0307480e953db951de758dff5a27aef8
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-07-13 21:03:59 +0200

    Setting dependencies, application properties and gitignore

---
+++ place-booker-backend/src/main/java/com/placebooker/placebooker/PlaceBookerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.placebooker.placebooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceBookerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceBookerBackendApplication.class, args);
+    }
+
+}
---
+++ place-booker-backend/src/test/java/com/placebooker/placebooker/PlaceBookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placebooker.placebooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceBookerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
