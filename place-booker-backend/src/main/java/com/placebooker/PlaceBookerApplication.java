615072b32fd1eab83098a974bf5b9ea5567c7d0d
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-07-13 21:20:27 +0200

    Changed folder structure

--- place-booker-backend/src/main/java/com/placebooker/PlaceBookerApplication.java
+++ place-booker-backend/src/main/java/com/placebooker/PlaceBookerApplication.java
@@ -1,4 +1,4 @@
-package com.placebooker.placebooker;
+package com.placebooker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- place-booker-backend/src/test/java/com/placebooker/PlaceBookerApplicationTests.java
+++ place-booker-backend/src/test/java/com/placebooker/PlaceBookerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.placebooker.placebooker;
+package com.placebooker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
