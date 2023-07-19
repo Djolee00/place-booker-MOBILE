de46a870aa3c71e84755517db58853d0f0c2bbe9
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-07-19 22:53:09 +0200

    FE: Added side menu and tabs for home page

--- place-booker-frontend/src/app/app.component.ts
+++ place-booker-frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  private authSub: Subscription;
+  private previousAuthState = false; //auth guard doesnt have time to check autologin
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnInit() {
+    this.authSub = this.authService.userIsAuthenticated.subscribe((isAuth) => {
+      if (!isAuth && this.previousAuthState !== isAuth) {
+        this.router.navigateByUrl('/auth');
+      }
+      this.previousAuthState = isAuth;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.authSub) {
+      this.authSub.unsubscribe();
+    }
+  }
 }
--- place-booker-frontend/src/app/places/places-routing.module.ts
+++ place-booker-frontend/src/app/places/places-routing.module.ts
@@ -5,17 +5,59 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then(
+                (m) => m.DiscoverPageModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./discover/place-details/place-details.module').then(
+                (m) => m.PlaceDetailsPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then((m) => m.OffersPageModule),
+          },
+          {
+            path: 'new',
+            loadChildren: () =>
+              import('./offers/new-offer/new-offer.module').then(
+                (m) => m.NewOfferPageModule
+              ),
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () =>
+              import('./offers/edit-offer/edit-offer.module').then(
+                (m) => m.EditOfferPageModule
+              ),
+          },
+        ],
+      },
+    ],
+  },
+  {
     path: '',
-    component: PlacesPage
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full',
   },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
 ];
 
 @NgModule({
